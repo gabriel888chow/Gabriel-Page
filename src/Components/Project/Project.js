1ef700a024842f2c2fb4652d5e503c7b6f520750
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-02-15 23:17:26 +0800

    commit project

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,8 +1,41 @@
 import React from 'react';
-
+import './Project.css';
+import {useState} from 'react';
 
 export default function Project() {
+  const [click, setClick] = useState(false);
+  const card = document.querySelector('.card');
+
+  function clickRotate() {
+    card.classList.toggle('rotated');
+  }
+  card.addEventListener('click', clickRotate);
+
   return (
-    <div>Project</div>
+    <div>
+      {/* <div>Project</div> */}
+      <div className='cardList'>
+        <div className='card'>
+          <div className='card-contents card-front'>
+            <h3>Project Name:</h3>
+              <h2>Chore-Door</h2>
+            <hr/>
+            <p>This is a Codecademy</p>
+          </div>
+            <div className='card-contents card-front'>
+              <h3>Project Name:</h3>
+                <h2>ww</h2>
+              <hr/>
+              <p>This is a Codecademy</p>
+            </div>
+              <div className='card-contents card-front'>
+                <h3>Project Name:</h3>
+                  <h2>Apple</h2>
+                <hr/>
+                <p>This is a Codecademy</p>
+              </div>
+        </div>
+      </div>
+    </div>
   )
 }
