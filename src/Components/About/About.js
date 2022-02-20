dc558bc1757cdaf2bcf9cb9a3af072c26d9d5031
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-02-20 18:26:06 +0800

    commit add scroll

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,52 +11,71 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import ImageSlider from '../Slider/ImageSlider';
 import { SliderData } from '../Slider/SliderData';
+import { Fade, Reveal, Flip, Slide } from 'react-reveal';
 
 export default function About() {
   return (
     <Container>
       <Row className='about'>
-        <Col className='title'>
+        <Col className='title col-8'>
+          <Fade left>
             <h1>GABRIEL CHOW</h1>
             <p> I joined an intensive Front-end Web developer course at Generation Hong Kong and it equipped me with the skills of HTML, CSS, JavaScript, React.</p>
             <Link className="btn" to="/projectPage"> Click my project </Link>
+          </Fade>
         </Col>
-        <Col className='photo photoColor col-3 d-flex justify-content-center'>
+        <Col className='photo photoColor col-4 d-flex justify-content-center'>
+          <Fade right>
             <img src={img} />
+          </Fade>
         </Col>
       </Row>
           <hr className='bar'></hr>
       <Row className='slider'>
-        <h1 className='d-flex justify-content-center' >Travel Photos</h1>
-        <p> I like to traval, when I saw the nice view, I will use mobile to take pictures. There are my pictures I was took. It is from Hong Kong, Japan and Taiwan.</p>
-        <ImageSlider slides={SliderData}  />
-      </Row>  
+        <Flip top>
+          <h1 className='d-flex justify-content-center' >Travel Photos</h1>
+          <p> I like to traval, when I saw the nice view, I will use mobile to take pictures. There are my pictures I was took. It is from Hong Kong, Japan and Taiwan.</p>
+        </Flip>
+        <Fade top>
+          <ImageSlider slides={SliderData}  />
+        </Fade>
+      </Row> 
+
           <hr className='bar'></hr>
 
         <Row className='aboutMe d-flex align-items-center' >
           <Col className='col-5 aboutColor '>
             <div className='aboutText'>
-              <h1>About Me</h1>
-              <p>Before I leaning "Front-End", I was working in inteior design, this is my drawing of my job.</p>
+              <Flip top>
+                <h1>About Me</h1>
+                <p>Before I leaning "Front-End", I was working in inteior design, this is my drawing of my job.</p>
+              </Flip>
             </div>
           </Col>
           <Col className='plan col-9 d-flex justify-content-end'>
-            <img src={floorPlan1} />
-            <img src={floorPlan2} />
-            <img src={floorPlan3} />
+            <Slide right>
+              <img src={floorPlan1} />
+              <img src={floorPlan2} />
+              <img src={floorPlan3} />
+            </Slide>
           </Col>
         </Row> 
+
         <Row className=' moblie'>
         <Row className=' aboutColor moblie'>
             <div className='aboutText'>
-              <h1>About Me</h1>
-              <p>Before I leaning "Front-End", I was working in inteior design, this is my drawing of my job.</p>
+              <Slide left>
+                <h1>About Me</h1>
+                <p>Before I leaning "Front-End", I was working in inteior design, this is my drawing of my job.</p>
+              </Slide>
             </div>
           </Row>
           <Row className='img'>
-            <img src={floorPlan4} />
-            <img src={floorPlan5} />
-            <img src={floorPlan6} />
+            <Slide bottom>
+              <img src={floorPlan4} />
+              <img src={floorPlan5} />
+              <img src={floorPlan6} />
+            </Slide>
           </Row>
         </Row>
     </Container>
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,36 +1,38 @@
 import React from 'react'
 import './Card.css';
 // import {Link} from 'react-router-dom';
+import { Bounce, Fade, Flip } from 'react-reveal';
 
 
 export default function Card({title, text, skill, url1, url2}) {
   return (
     <div>
-      {/* <div>Project</div> */}
-      <div className='cardList container'>
-        <div className='card '>
-          <div className='card-contents card-front front-face'>
-            <div className='card-3d'>
-                <h6>Project Name:</h6>
-                  <h2>{title}</h2>
-                <hr/>
-                <p>{text}</p>
-                <br/>
-                <p>{skill}</p>
+      <Fade bottom>
+        <div className='cardList container'>
+          <div className='card '>
+            <div className='card-contents card-front front-face'>
+              <div className='card-3d'>
+                  <h6>Project Name:</h6>
+                    <h2>{title}</h2>
+                  <hr/>
+                  <p>{text}</p>
+                  <br/>
+                  <p>{skill}</p>
+              </div>
             </div>
-          </div>
-          {/* card back  */}
-          <div className='card-contents card-back back-face'>
-            <div className='card-3d'>
-              <h2>Chack my link</h2>
-              <div>
-                <a href={url1} className="btn btn-outline-light m-2"> Github </a>
-                <a href={url2} className="btn btn-outline-light m-2"> Link Page </a>
+            {/* card back  */}
+            <div className='card-contents card-back back-face'>
+              <div className='card-3d'>
+                <h2>Chack my link</h2>
+                <div>
+                  <a href={url1} className="btn btn-outline-light m-2"> Github </a>
+                  <a href={url2} className="btn btn-outline-light m-2"> Link Page </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Fade>
     </div>
   )
 }
