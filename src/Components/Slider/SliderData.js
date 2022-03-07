5aa778b0dc62871eebcb65484068e57049491357
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-03-07 19:37:23 +0800

    commit

--- src/Components/Slider/SliderData.js
+++ src/Components/Slider/SliderData.js
@@ -1,22 +1,22 @@
 export const SliderData = [
     {
       image:
-        'https://scontent.fhkg1-1.fna.fbcdn.net/v/t39.30808-6/274305358_10159731627522645_8383958661285094762_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=730e14&_nc_ohc=eFRsV7ua63gAX_WcS5J&_nc_ht=scontent.fhkg1-1.fna&oh=00_AT_5rPeWqJ2yP_Xt0StTRP9q8c9bx8gnQN6D8mmAsl9VWQ&oe=62263B20'
+        'https://scontent.fhkg4-2.fna.fbcdn.net/v/t39.30808-6/274305358_10159731627522645_8383958661285094762_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=730e14&_nc_ohc=PRsx3LhODnwAX-GfvxZ&_nc_ht=scontent.fhkg4-2.fna&oh=00_AT_eGf3VFcF2sHD0AVsQ1Yw5NUHIyWGL2bwUtXyRzU21eQ&oe=622A2FA0'
     },
     {
       image:
-        'https://scontent.fhkg1-1.fna.fbcdn.net/v/t39.30808-6/274234146_10159731629162645_7230094967567178697_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=pF5YhVi2vx0AX9q2kWh&_nc_ht=scontent.fhkg1-1.fna&oh=00_AT91wID9uaf10Iy4vo8Vgv84dZUNk0gaGpv1TfZkk4Kf0A&oe=6224C13B'
+        'https://scontent.fhkg4-1.fna.fbcdn.net/v/t39.30808-6/274234146_10159731629162645_7230094967567178697_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=87LCUNNCcK0AX_5sTW3&_nc_ht=scontent.fhkg4-1.fna&oh=00_AT_cKTWC6G8qxh_y6wAhCIKmVog87c-A7f0LTe8X8ub1cA&oe=622AAFFB'
     },
     {
       image:
-        'https://scontent.fhkg1-1.fna.fbcdn.net/v/t39.30808-6/274033839_10159731629262645_7423270865450926521_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=7oFm088Q4EIAX-IXndk&_nc_ht=scontent.fhkg1-1.fna&oh=00_AT_e0YsGfe89Jyzvr7-ST4RpF9nxRECpVIIXWu2qdKJ1bg&oe=6225DFB8'
+        'https://scontent.fhkg4-1.fna.fbcdn.net/v/t39.30808-6/274033839_10159731629262645_7423270865450926521_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=YgoFi9I4RFgAX-PAy4N&_nc_ht=scontent.fhkg4-1.fna&oh=00_AT8q1XhLFXAoSwuGoErTZ7Sc8fzkTRJRP_dhh7slUhO3zg&oe=622BCE78'
     },
     {
       image:
-        'https://scontent.fhkg1-1.fna.fbcdn.net/v/t39.30808-6/274173412_10159731629247645_8235643678915949140_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=PBJ_4-GPyjsAX-I4PzE&_nc_ht=scontent.fhkg1-1.fna&oh=00_AT9ez9Ajqbw1gXqdKKlLM_9arDJgBKBcBP3EvLomG8FAFg&oe=6225CD4E'
+        'https://scontent.fhkg4-2.fna.fbcdn.net/v/t39.30808-6/274173412_10159731629247645_8235643678915949140_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=eLviX5mHCZUAX_PUtL3&_nc_ht=scontent.fhkg4-2.fna&oh=00_AT8wfAT4gZPXdlM5k0eyaEdwJYZp0Lq2fFjQxuyzW8aZGQ&oe=622BBC0E'
     },
     {
       image:
-        'https://scontent.fhkg1-1.fna.fbcdn.net/v/t39.30808-6/274160942_10159731629127645_4884760078519376138_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=3iWWjaDk6zAAX-6Vxeg&_nc_oc=AQn4puLD2X8DZ__Xn-FucNY7tRMU8VTX7I4Jbzvce1DApSPW5a9W0d-IKwuajvlzxWY&tn=kfzXTAa-fiJq7c3x&_nc_ht=scontent.fhkg1-1.fna&oh=00_AT8sepPCSHo9WSeb13_6cWtMVmN0RI4tuio0pkfKbRR0Yw&oe=62263374'
+        'https://scontent.fhkg4-2.fna.fbcdn.net/v/t39.30808-6/274160942_10159731629127645_4884760078519376138_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=0-uISyrL578AX91Qyhp&_nc_oc=AQlQV1E5NXwbysPAAhNDjwon-Iwp7FPfdV-sUMc15DqS_715lscrP6-Gh2BBUrZUea0&tn=kfzXTAa-fiJq7c3x&_nc_ht=scontent.fhkg4-2.fna&oh=00_AT_uBMg7D4pMNNUclUWo8EUfWGv-4qvYf5DVkx0q5CGtog&oe=622A27F4'
     }
   ];
